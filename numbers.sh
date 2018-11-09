f186911310bd5336d052a4f80888e19fff1668f0
Ryan Millares <rmillares@chapman.edu>
2018-11-09 14:01:40 -0800

    Adds numbers.sh

--- numbers.sh
+++ numbers.sh
@@ -2,23 +2,37 @@
 # numbers.sh
 # Ryan Millares
 
-echo "Please put a number from 1 to 9!"
-read NUM
-while echo $NUM | egrep -v "^[1-9]{1}$" > /dev/null 2>&1
+echo "Begin? y/n"
+read ANS
+while [ "$ANS" = "y" ]
 do
-	echo "Must be a number from 1 to 9!"
-	echo "Please try again."
+	echo "Please put a positive number!"
 	read NUM
+	while echo $NUM | egrep -v "^[1-9]{1}$" > /dev/null 2>&1
+	do
+		echo "Must be a number from 1 to 9!"
+		echo "Please try again."
+		read NUM
+	done
+	
+	Z=1
+	while [ "$Z" -le "$NUM" ]
+	do
+		if [ "$((Z % 2))" -eq 0 ] 
+		then
+			echo "$Z Even"
+		else 
+			echo "$Z Odd"
+		fi
+		Z=$((Z+1))
+	
+		
+	done 
+	echo "Thank you!"
+	echo "Want to do another?"
+	read ANS
 done
-declare -i COUNT = 0
-while "$COUNT" -le "$NUM"
-do
-	if [$(($COUNT%2)) -eq 0]
-	then
-		echo "$COUNT Even"
-	else 
-		echo "$COUNT Odd"
-	fi
-	N=$((COUNT+1))
-done 
-echo "Thank you!"
+echo "All done!"
+# last read is to pause program before end, was used to read error msgs before program closes
+read stop
+
