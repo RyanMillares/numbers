ec42456e6aa2ab0d095c513ac44b7bae8ac35b84
Ryan Millares <rmillares@chapman.edu>
2018-11-12 11:30:48 -0800

    Adds numbers.sh

--- numbers.sh
+++ numbers.sh
@@ -3,6 +3,7 @@
 # Ryan Millares
 
 echo "Begin? y/n"
+#to initiate the variable; could not declare with value properly so left as a read
 read ANS
 while [ "$ANS" = "y" ]
 do
@@ -10,7 +11,7 @@ do
 	read NUM
 	while echo $NUM | egrep -v "^[1-9]{1}$" > /dev/null 2>&1
 	do
-		echo "Must be a number from 1 to 9!"
+		echo "Must be a  positive number between 1 to 9!"
 		echo "Please try again."
 		read NUM
 	done
@@ -24,6 +25,7 @@ do
 		else 
 			echo "$Z Odd"
 		fi
+		#incrementor
 		Z=$((Z+1))
 	
 		
